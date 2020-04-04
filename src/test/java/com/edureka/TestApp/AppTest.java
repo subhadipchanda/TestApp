fa41e17f958e168466e38cc0dd7642740c0c93cf
pbeniwal <pbeniwal>
2020-04-04 11:47:07 +0530

    udated test case

--- src/test/java/com/edureka/TestApp/AppTest.java
+++ src/test/java/com/edureka/TestApp/AppTest.java
@@ -1,20 +1,44 @@
 package com.edureka.TestApp;
 
-import static org.junit.Assert.assertTrue;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	void testHelloEdureka() throws IOException {
+		
+	    WebDriver driver;
+	    
+        String mygecko=System.getenv("HOME") + "/Downloads/geckodriver";
+        
+        System.setProperty("webdriver.gecko.driver",mygecko);
+        
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("http://localhost:8080/helloedureka");
+        
+		
+	    String text = "Hello from Edureka";
+	    
+	    int l = text.length();
+
+	    String message = "Expected String " + text +  " not found";
+	    
+	    String bodyText = driver.findElement(By.tagName("body")).getText();
+
+	    System.out.println("Body: " + bodyText.substring(0, l ) + ".");
+        Assert.assertEquals(bodyText.substring(0, l),text, message);
+        driver.quit();
+	}
+
+
 }
