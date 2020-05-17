9df8d5ab54c4d5aa06bc333408839ab39b2bb5a9
pbeni <pbeni@PRASHANT>
2020-05-17 18:45:10 +0530

    updated

--- src/test/java/com/edureka/TestApp/AppTest.java
+++ src/test/java/com/edureka/TestApp/AppTest.java
@@ -26,9 +26,9 @@ public class AppTest
 	    
         String mygecko=System.getenv("HOME") + "/Downloads/geckodriver";
 
-	System.out.println("Opening http://34.72.56.228:9090/helloedureka");
+	    System.out.println("Opening http://34.72.56.228:9090/helloedureka");
 
-	System.out.println(mygecko);
+	    System.out.println(mygecko);
 
         System.setProperty("webdriver.gecko.driver",mygecko);
         
@@ -36,25 +36,20 @@ public class AppTest
         
         driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
 
-	System.out.println("Opening http://34.72.56.228:9090/helloedureka");
+	    System.out.println("Opening http://34.72.56.228:9090/helloedureka");
 
-	// You will need to change localhost to IP in case of AWS
+	    // You will need to change localhost to IP in case of AWS
 
         driver.get("http://34.72.56.228:9090/helloedureka");
         
         Thread.sleep(5000);
         
 	    String text = "Hello from Edureka";
-	    
 	    int l = text.length();
-
-	    String message = "Expected String " + text +  " not found";
-	    
-	    String bodyText = driver.findElement(By.tagName("body")).getText();
-
-	    System.out.println("bgcolor: " + driver.findElement(By.tagName("body")).getAttribute("bgcolor"));
-       
-	    Assert.assertEquals(bodyText.substring(0, l),text, message);
+        //String message = "Expected String " + text +  " not found";
+        String bodyText = driver.findElement(By.tagName("body")).getText();
+        
+	    Assert.assertEquals(bodyText.substring(0, l),text);
         
 	    WebElement mytext = driver.findElement(By.xpath("/html/body/h3"));
 	    
@@ -62,7 +57,7 @@ public class AppTest
 	    
         String mycolor = driver.findElement(By.tagName("body")).getAttribute("bgcolor");
 	    
-        String col_message = "Expected bgcolor to be Aqua but found " + mycolor;
+        //String col_message = "Expected bgcolor to be Aqua but found " + mycolor;
 	    
 	    Assert.assertEquals(mycolor, "Aqua");
         
